7803bfbdd3525bae964b26c6fbd5b325a8919881
ticktakclock <ticchettio.di.un.orologio@gmail.com>
2020-01-26 22:22:28 +0900

    Add activity transition

---
+++ app/src/main/java/com/github/ticktakclock/bottomnavigationsample/SubActivity.kt
@@ -0,0 +1,12 @@
+package com.github.ticktakclock.bottomnavigationsample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
