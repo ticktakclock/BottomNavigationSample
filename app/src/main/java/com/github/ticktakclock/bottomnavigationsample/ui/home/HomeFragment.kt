a38723fc4bbda299695fac92b210f8820ef583cf
ticktakclock <ticchettio.di.un.orologio@gmail.com>
2020-01-26 21:19:50 +0900

    Add transition animation

--- app/src/main/java/com/github/ticktakclock/bottomnavigationsample/ui/home/HomeFragment.kt
+++ app/src/main/java/com/github/ticktakclock/bottomnavigationsample/ui/home/HomeFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.github.ticktakclock.bottomnavigationsample.R
 
 class HomeFragment : Fragment() {
@@ -26,6 +28,10 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+        val button: Button = root.findViewById(R.id.button)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_home_to_navigation_dashboard)
+        }
         return root
     }
 }
